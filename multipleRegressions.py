1bc0c8f80869fae9e185cb89f7cf70dbaa45d0ed
Gene <gene.burinsky@gmail.com>
2019-09-20 22:11:46 -0700

    added star notation

--- multipleRegressions.py
+++ multipleRegressions.py
@@ -15,13 +15,15 @@ def runQuantRegression(y,x,data,quant=.5):
     res = mod.fit(quant=.5)
     return(res)
 
-def runRegressions(y, xList, key_vars, data, specification_names=None, cov_type='HC0'):
+def runRegressions(y, xList, key_vars, data, specification_names=None, cov_type='HC0', pvalues=True):
     """
     y - dependent variable (string)
     xList - list of explanatory variable strings (list of Patsy compatible formulas)
     key_vars - list of independent variables you wish to see results for
     df - a Pandas DataFrame that contains the data (list)
     specification_names - list of regression names (list of strings)
+    cov_type - type of covariance you wish to estimate the regression with (default is robust)
+    pvalues - whether you want to report parameter estimates with p-values or star notation (default p-values)
     
     Description: runRegressions estimates multiple regression specifications
     and returns coefficent and p-values estimates for key_vars and regressions
@@ -49,9 +51,12 @@ def runRegressions(y, xList, key_vars, data, specification_names=None, cov_type=
         outDict[key] = {'nObs':regDict[key].nobs,'R^2 adj':regDict[key].rsquared_adj.round(3), 'cond. num':regDict[key].condition_number}
         
         # loop over variables of interest
-        for var in key_vars:
-            outDict[key][var] = "%.04f (%.03f)" %(regDict[key].params[var], regDict[key].pvalues[var])
-        
+        if pvalues:
+            for var in key_vars:
+                outDict[key][var] = "%.04f (%.03f)" %(regDict[key].params[var], regDict[key].pvalues[var])
+        if pvalues == False:
+            outDict[key] = regDict[key].params.round(3).astype(str) + pd.cut(regDict[key].pvalues,bins=[0,.01,.05,.1,1], labels=['***','**','*','']).astype(str)
+            
     outdf =pd.DataFrame(outDict).transpose()
     outdf.index.name='specification'
     return(outdf)   
